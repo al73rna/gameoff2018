6b2d914d3b4ed4d2236dc9047653dd3c2ae3c94e
Mohammad Reza Brz <al73rna@gmail.com>
2017-11-11 22:13:16 -0800

    test assets

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+	public GameObject Camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void setCameraPosition(){
+		Camera.transform.position = new Vector3(transform.position.x,Camera.transform.position.y,-10);
+	}
+	// Update is called once per frame
+	void Update () {
+		setCameraPosition();
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	float time;
+	bool isGrounded;
+	// Update is called once per frame
+	void Update () {
+
+		// if (Physics2D.Raycast (transform.position - new Vector3 (0, 0.6f, 0), Vector2.down, 0.1f)) {
+		// 	isGrounded = true;
+		// } else {
+		// 	isGrounded = false;
+		// }
+		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.1f) {
+				GetComponent<Rigidbody2D> ().AddForce(new Vector2 (Input.GetAxis ("Horizontal") * 10, 0)) ;
+
+		}
+
+		if (Input.GetKey ("space")) {
+
+			if (isGrounded) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, 10);
+			}
+
+		}
+	}
+	private void OnCollisionEnter2D(Collision2D other) {
+	if(other.gameObject.CompareTag("ground")){
+			isGrounded=true;
+		}	
+	}
+	private void OnCollisionExit2D(Collision2D other) {
+		if(other.gameObject.CompareTag("ground")){
+			isGrounded=false;
+		}
+	}
+}
